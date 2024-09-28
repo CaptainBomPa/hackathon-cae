7926b80476767b8c8063be521ea2db862fed0f23
MK <marcinkalaus@gmail.com>
2024-09-28 21:30:55 +0200

    check this shit

--- backend/src/main/java/com/example/websocket/config/WebConfig.java
+++ backend/src/main/java/com/example/websocket/config/WebConfig.java
@@ -10,10 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // This allows all origins
+                .allowedOrigins("http://localhost") // This allows all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
                 .allowedHeaders("*") // Allow all headers
-                .allowCredentials(false) // If you don't need cookies, set this to false
+                .allowCredentials(true) // If you don't need cookies, set this to false
                 .maxAge(3600); // Set max age for preflight requests
     }
 }
