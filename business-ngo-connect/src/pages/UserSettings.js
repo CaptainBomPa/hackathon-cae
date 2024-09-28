ad9f5025d0f30937bd01b0f10900244772914910
Anna Franczyk <anamonium@gmail.com>
2024-09-28 23:10:29 +0200

    fix fix fix

--- business-ngo-connect/src/pages/UserSettings.js
+++ business-ngo-connect/src/pages/UserSettings.js
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Box, Typography, Button, TextField, Grid, Paper, Alert, Snackbar, Avatar, IconButton } from '@mui/material';
+import {
+  Container,
+  Box,
+  Typography,
+  Button,
+  TextField,
+  Grid,
+  Paper,
+  Alert,
+  Snackbar,
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+} from '@mui/material';
+import { motion } from 'framer-motion';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
 import DashboardLayout from '../components/DashboardLayout';
 import { updateProfile, updatePassword, getUser } from '../services/userService';
-import { getUserPhoto, updateUserPhoto } from '../services/photoService'; // Import funkcji do obsługi zdjęć
-import { motion } from 'framer-motion'; // Import framer-motion
-import PhotoCamera from '@mui/icons-material/PhotoCamera'; // Ikona do przesyłania zdjęcia
+import { getUserPhoto, updateUserPhoto } from '../services/photoService';
+import CustomTextField from '../components/CustomTextField';
 
-// Definicja animacji
 const pageVariants = {
-  initial: { opacity: 0, y: 50 }, // Stan początkowy: niewidoczny, przesunięty w dół
-  animate: { opacity: 1, y: 0 }, // Stan końcowy: widoczny, na właściwej pozycji
-  exit: { opacity: 0, y: 50 } // Stan podczas wychodzenia: niewidoczny, przesunięty w dół
+  initial: { opacity: 0, y: 50 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 50 },
 };
+
+const filterOptions = [
+  { label: 'Edit Profile', value: 'profile' },
+  { label: 'Edit Photo', value: 'photo' },
+  { label: 'Change Password', value: 'password' },
+];
 
 const UserSettingsPage = () => {
   const [userData, setUserData] = useState({
@@ -19,23 +40,21 @@ const UserSettingsPage = () => {
     email: '',
     photoUrl: '',
   });
-  const [loggedInUserId, setLoggedInUserId] = useState(null); // Przechowywanie ID zalogowanego użytkownika
+  const [loggedInUserId, setLoggedInUserId] = useState(null);
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [alertMessage, setAlertMessage] = useState('');
   const [showAlert, setShowAlert] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null); // Przechowywanie wybranego zdjęcia
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [activeTab, setActiveTab] = useState('profile');
 
-  // Przy pobraniu danych z localStorage zakładamy, że klucz 'user' przechowuje informacje o zalogowanym użytkowniku
   useEffect(() => {
     const fetchUserData = async () => {
       const storedUser = localStorage.getItem('user');
       if (storedUser) {
         const user = JSON.parse(storedUser);
-        setLoggedInUserId(user.id); // Ustawienie ID użytkownika z localStorage
-        
-        // Pobieranie danych użytkownika
+        setLoggedInUserId(user.id);
         setUserData({
           ...userData,
           name: user.name || '',
@@ -43,18 +62,18 @@ const UserSettingsPage = () => {
         });
 
         try {
-          const photoUrl = await getUserPhoto(user.id); // Pobieranie zdjęcia użytkownika
-          setUserData((prevData) => ({ ...prevData, photoUrl })); // Aktualizacja URL zdjęcia
+          const photoUrl = await getUserPhoto(user.id);
+          setUserData((prevData) => ({ ...prevData, photoUrl }));
         } catch (error) {
           console.error('Error fetching user photo:', error);
         }
       }
     };
     fetchUserData();
-  }, []); // Pusty array dependencies oznacza, że useEffect wykona się tylko raz po montażu komponentu
+  }, []);
 
   const handleProfileUpdate = async () => {
-    if (!loggedInUserId) return; // Jeśli brak ID użytkownika, nie wykonujemy operacji
+    if (!loggedInUserId) return;
 
     try {
       await updateProfile(loggedInUserId, { name: userData.name, email: userData.email });
@@ -67,7 +86,7 @@ const UserSettingsPage = () => {
   };
 
   const handlePasswordUpdate = async () => {
-    if (!loggedInUserId) return; // Jeśli brak ID użytkownika, nie wykonujemy operacji
+    if (!loggedInUserId) return;
 
     if (newPassword !== confirmPassword) {
       setAlertMessage('Passwords do not match.');
@@ -93,16 +112,15 @@ const UserSettingsPage = () => {
   };
 
   const handlePhotoUpload = async () => {
-    if (!loggedInUserId || !selectedPhoto) return; // Jeśli brak ID użytkownika lub zdjęcia, nie wykonujemy operacji
+    if (!loggedInUserId || !selectedPhoto) return;
 
     try {
       const updatedUser = await updateUserPhoto(loggedInUserId, selectedPhoto);
-      setUserData(updatedUser); // Aktualizacja danych użytkownika z nowym zdjęciem
+      setUserData(updatedUser);
       setAlertMessage('Profile photo updated successfully.');
       setShowAlert(true);
-      setSelectedPhoto(null); // Zresetowanie wybranego zdjęcia
+      setSelectedPhoto(null);
 
-      // Pobranie nowego zdjęcia po aktualizacji
       const photoUrl = await getUserPhoto(loggedInUserId);
       setUserData((prevData) => ({ ...prevData, photoUrl }));
     } catch (error) {
@@ -111,139 +129,226 @@ const UserSettingsPage = () => {
     }
   };
 
+  const renderActiveTab = () => {
+    switch (activeTab) {
+      case 'profile':
+        return (
+          <Paper
+            sx={{
+              padding: 2,
+              backgroundColor: '#f0f0f0', // Gray background
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center', // Center items
+              borderRadius: '20px',
+            }}
+          >
+            <Typography variant="h6" gutterBottom>
+              Update User Information
+            </Typography>
+            <TextField
+              label="Name"
+              fullWidth
+              value={userData.name}
+              onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+              sx={{ marginBottom: 2,
+                borderRadius: '20px',
+               }}
+            />
+            <TextField
+              label="Email"
+              fullWidth
+              value={userData.email}
+              onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+              sx={{ 
+                marginBottom: 2,
+                '& .MuiInputLabel-root': {
+                  fontSize: '18px', // Zwiększenie rozmiaru etykiety
+                },
+               }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleProfileUpdate}
+              sx={{ 
+                borderRadius: '20px',
+                marginTop: 2, 
+                backgroundColor: 'rgba(65, 140, 181, 0.8)', 
+                '&:hover': { backgroundColor: 'rgba(65, 140, 181, 1.0)' } }}
+              fullWidth
+            >
+              Update Profile
+            </Button>
+          </Paper>
+        );
+      case 'password':
+        return (
+          <Paper
+            sx={{
+              padding: 2,
+              backgroundColor: '#f0f0f0', // Gray background
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center', // Center items
+              borderRadius: '20px',
+            }}
+          >
+            <Typography variant="h6" gutterBottom>
+              Change Password
+            </Typography>
+            <TextField
+              label="Current Password"
+              type="password"
+              fullWidth
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+              sx={{ marginBottom: 2 }}
+            />
+            <TextField
+              label="New Password"
+              type="password"
+              fullWidth
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+              sx={{ marginBottom: 2 }}
+            />
+            <TextField
+              label="Confirm New Password"
+              type="password"
+              fullWidth
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              sx={{ marginBottom: 2 }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handlePasswordUpdate}
+              sx={{ marginTop: 2, backgroundColor: 'rgba(65, 140, 181, 0.8)', '&:hover': { backgroundColor: 'rgba(65, 140, 181, 1.0)' } }}
+              fullWidth
+            >
+              Change Password
+            </Button>
+          </Paper>
+        );
+      case 'photo':
+        return (
+          <Paper
+            sx={{
+              padding: 2,
+              backgroundColor: '#f0f0f0', // Gray background
+              borderRadius: '20px',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center', // Center items
+            }}
+          >
+            <Typography variant="h6" gutterBottom>
+              Update Profile Photo
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 2 }}>
+              <Avatar src={userData.photoUrl} sx={{ width: 120, height: 120, marginRight: 2 }} />
+              <IconButton
+                color="primary"
+                component="label"
+                sx={{ backgroundColor: '#82ff82', '&:hover': { backgroundColor: '#70e270' } }}
+              >
+                <PhotoCamera />
+                <input
+                  type="file"
+                  hidden
+                  onChange={(e) => setSelectedPhoto(e.target.files[0])}
+                />
+              </IconButton>
+            </Box>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handlePhotoUpload}
+              sx={{ marginTop: 2, backgroundColor: 'rgba(65, 140, 181, 0.8)', '&:hover': { backgroundColor: 'rgba(65, 140, 181, 1.0)' } }}
+              fullWidth
+              disabled={!selectedPhoto}
+            >
+              Upload Photo
+            </Button>
+          </Paper>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <DashboardLayout>
-      <motion.div
-        variants={pageVariants}
-        initial="initial"
-        animate="animate"
-        exit="exit"
-        transition={{ duration: 0.5 }} // Czas trwania animacji
-      >
-        <Container maxWidth="lg" sx={{ marginTop: 2 }}>
-          <Grid container spacing={4}>
-            {/* Lewa strona: Edycja danych użytkownika */}
-            <Grid item xs={12} md={6}>
-              <Paper sx={{ padding: 2 }}>
-                <Typography variant="h6" gutterBottom>
-                  Update User Information
-                </Typography>
-                <TextField
-                  label="Name"
-                  fullWidth
-                  value={userData.name}
-                  onChange={(e) => setUserData({ ...userData, name: e.target.value })}
-                  sx={{ marginBottom: 2 }}
-                />
-                <TextField
-                  label="Email"
-                  fullWidth
-                  value={userData.email}
-                  onChange={(e) => setUserData({ ...userData, email: e.target.value })}
-                  sx={{ marginBottom: 2 }}
-                />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleProfileUpdate}
-                  sx={{ marginTop: 2, backgroundColor: '#82ff82', '&:hover': { backgroundColor: '#70e270' } }}
-                  fullWidth
-                >
-                  Update Profile
-                </Button>
-              </Paper>
-            </Grid>
-
-            {/* Prawa strona: Zmiana hasła */}
-            <Grid item xs={12} md={6}>
-              <Paper sx={{ padding: 2 }}>
-                <Typography variant="h6" gutterBottom>
-                  Change Password
-                </Typography>
-                <TextField
-                  label="Current Password"
-                  type="password"
-                  fullWidth
-                  value={currentPassword}
-                  onChange={(e) => setCurrentPassword(e.target.value)}
-                  sx={{ marginBottom: 2 }}
-                />
-                <TextField
-                  label="New Password"
-                  type="password"
-                  fullWidth
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                  sx={{ marginBottom: 2 }}
-                />
-                <TextField
-                  label="Confirm New Password"
-                  type="password"
-                  fullWidth
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  sx={{ marginBottom: 2 }}
-                />
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handlePasswordUpdate}
-                  sx={{ marginTop: 2, backgroundColor: '#82ff82', '&:hover': { backgroundColor: '#70e270' } }}
-                  fullWidth
-                >
-                  Change Password
-                </Button>
-              </Paper>
-            </Grid>
-
-            {/* Dodatkowy panel: Aktualizacja zdjęcia profilowego */}
-            <Grid item xs={12}>
-              <Paper sx={{ padding: 2 }}>
-                <Typography variant="h6" gutterBottom>
-                  Update Profile Photo
-                </Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: 2 }}>
-                  <Avatar src={userData.photoUrl} sx={{ width: 120, height: 120, marginRight: 2 }} />
-                  <IconButton
-                    color="primary"
-                    component="label"
-                    sx={{ backgroundColor: '#82ff82', '&:hover': { backgroundColor: '#70e270' } }}
-                  >
-                    <PhotoCamera />
-                    <input
-                      type="file"
-                      hidden
-                      onChange={(e) => setSelectedPhoto(e.target.files[0])}
-                    />
-                  </IconButton>
-                </Box>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handlePhotoUpload}
-                  sx={{ marginTop: 2, backgroundColor: '#418cb5', '&:hover': { backgroundColor: '#3179a3' } }}
-                  fullWidth
-                  disabled={!selectedPhoto} // Disable button if no photo selected
-                >
-                  Upload Photo
-                </Button>
-              </Paper>
-            </Grid>
-          </Grid>
-
-          {/* Alert */}
-          <Snackbar
-            open={showAlert}
-            autoHideDuration={5000}
-            onClose={handleCloseAlert}
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      <Container maxWidth="lg" sx={{ marginTop: 2 }}>
+        <Grid container spacing={4}>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              width: '12%',
+              height: '100%',
+              backgroundColor: 'rgba(109, 109, 109, 0.4)',
+              padding: 0,
+              marginTop: "64px",
+              marginRight: 4,
+              position: 'fixed',
+              left: 0,
+              top: 0,
+            }}
           >
-            <Alert onClose={handleCloseAlert} severity="info" sx={{ width: '100%' }}>
-              {alertMessage}
-            </Alert>
-          </Snackbar>
-        </Container>
-      </motion.div>
+            <List sx={{ width: '100%' }}>
+              {filterOptions.map((option, index) => (
+                <ListItem
+                  key={index}
+                  button
+                  onClick={() => setActiveTab(option.value)}
+                  sx={{
+                    backgroundColor:
+                      activeTab === option.value
+                        ? 'rgba(65, 140, 181, 0.6)'
+                        : 'rgba(109, 109, 109, 0.0)',
+                    '&:hover': {
+                      backgroundColor: 'rgba(109, 109, 109, 0.6)',
+                    },
+                    opacity: 'inherit',
+                    transition: 'background-color 0.3s ease',
+                  }}
+                >
+                  <ListItemText primary={option.label} sx={{ color: 'black' }} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+
+          <Grid item xs={12} md={9} sx={{ 
+            marginLeft: '20%' 
+            }}> {/* Add margin to offset the fixed sidebar */}
+            <motion.div
+              variants={pageVariants}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              transition={{ duration: 0.5 }}
+            >
+              {renderActiveTab()}
+            </motion.div>
+          </Grid>
+        </Grid>
+
+        <Snackbar
+          open={showAlert}
+          autoHideDuration={5000}
+          onClose={handleCloseAlert}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+          <Alert onClose={handleCloseAlert} severity="info" sx={{ width: '100%' }}>
+            {alertMessage}
+          </Alert>
+        </Snackbar>
+      </Container>
     </DashboardLayout>
   );
 };
