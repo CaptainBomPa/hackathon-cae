0006cf3c076caf71af7b6a0b6f5de24803945840
Filip Mróz <filip.mroz12@gmail.com>
2024-09-29 05:10:43 +0200

    eeeeeee makarena

--- business-ngo-connect/src/services/mainService.js
+++ business-ngo-connect/src/services/mainService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://0.0.0.0:5000/recommendations';
+const API_BASE_URL = 'http://localhost:8080/recommendations';
 
 // Funkcja do pobierania rekomendacji NGO dla firm
 export const getCompanyRecommendations = async (userId) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/companies`, {
+    const response = await axios.get(`${API_BASE_URL}/ngo/companies`, {
       params: { id: userId },
     });
     return response.data;
@@ -18,7 +18,9 @@ export const getCompanyRecommendations = async (userId) => {
 // Funkcja do pobierania rekomendacji NGO dla wolontariuszy
 export const getVolunteerRecommendations = async (userId) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/volunteers`);
+    const response = await axios.get(`${API_BASE_URL}/ngo/volunteers`, {
+        params: { id: userId },
+      });
     return response.data;
   } catch (error) {
     console.error('Error fetching volunteer recommendations:', error);
@@ -29,7 +31,9 @@ export const getVolunteerRecommendations = async (userId) => {
 // Funkcja do pobierania rekomendacji firm dla NGO
 export const getNgoCompanyRecommendations = async (userId) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/companies`);
+    const response = await axios.get(`${API_BASE_URL}/company`, {
+        params: { id: userId },
+      });
     return response.data;
   } catch (error) {
     console.error('Error fetching NGO company recommendations:', error);
@@ -40,7 +44,9 @@ export const getNgoCompanyRecommendations = async (userId) => {
 // Funkcja do pobierania rekomendacji wolontariuszy dla NGO
 export const getNgoVolunteerRecommendations = async (userId) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/ngo`);
+    const response = await axios.get(`${API_BASE_URL}/ngo`, {
+        params: { id: userId },
+      });
     return response.data;
   } catch (error) {
     console.error('Error fetching NGO volunteer recommendations:', error);
