6e19c0d9e8feb2da4bc6c5ccd251d922421997c7
ajablonsk1 <adrian.tomasz.jablonski@gmail.com>
2024-09-29 02:26:05 +0200

    Fixed port

--- recommendation-system/src/main.py
+++ recommendation-system/src/main.py
@@ -48,4 +48,4 @@ def get_ngos_for_volunteer():
 if __name__ == '__main__':
     DATABASE_URL = os.getenv('DATABASE_URL')
     db = Database(DATABASE_URL)
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
